f9cd523531b73dc4c76e9945e720afa2653cf8a9
Denis Atkesone <denis.atkesone@gmail.com>
2017-10-31 19:15:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ function createRock(x) {
          window.requestAnimationFrame(moveRock);
        } else {
          ROCKS.shift();
-         rock.parentElement.removeChild(rock);
+         rock.remove();
        }
      }
   }
